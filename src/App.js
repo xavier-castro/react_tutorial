c146fc3f2ecf244814e9fe0043ebe4e97eb00360
xavier-castro <xavier_castro@icloud.com>
2019-07-16 00:10:19 -0500

    Make Game main parent
    Co-authored-by: collinreal <collinreal.acc@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,46 +3,29 @@ import { Button, Card, Elevation } from "@blueprintjs/core";
 
 import "./App.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// The Square component is now considered a coontrolled component because the Board has full control over it
+function Square(props) {
+  return (
+    <Button className="square" onClick={props.onClick}>
+      {props.value}
+    </Button>
+  );
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
-  }
-
   renderSquare(i) {
     return (
       // We are now passing 2 props from Board to Square: (1): value and (2): onClick
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = "Next player: X";
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -64,14 +47,51 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    // .slice() is used to create a copy of squares array
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -83,7 +103,7 @@ function App() {
   return (
     <div className="appContainer">
       <div className="cardContainer">
-        <Card interactive={true} elevation={Elevation.ONE} className="grow">
+        <Card interactive={true} elevation={Elevation.ONE}>
           <h1>
             <a href="https://reactjs.org/tutorial/tutorial.html">
               Intro to React & BlueprintJS
@@ -94,11 +114,31 @@ function App() {
             state, and child to parent relationships via creation of a
             tic-tac-toe game.
           </p>
-          <Button icon="star" text="Begin Game" intent="success" />
+          <Game />
         </Card>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
